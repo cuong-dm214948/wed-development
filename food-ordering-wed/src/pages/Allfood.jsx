4035ad641c2a997c080ddeacccf472f396ef8e2c
cuong-dm214948 <cuong.dm214948@sis.hust.edu.vn>
2024-03-25 19:53:30 +0700

    first commit

---
+++ food-ordering-wed/src/pages/Allfood.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Helmet from "../components/Helmet/Helmet"
+import CommonSection from '../components/UI/common-section/CommonSection'
+
+
+
+const Allfood = () => {
+  return (
+    <Helmet title= 'All-foods'>
+        <CommonSection title= 'All food'></CommonSection>
+    </Helmet>
+  )
+}
+
+export default Allfood
